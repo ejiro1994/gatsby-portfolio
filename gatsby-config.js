2681b667d2103f62b5ac611a67fa34f7381185ed
ejiro <ejiro1994@gmail.com>
2021-03-09 13:20:01 +0000

    title page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ejiro Lennox`,
+    description: `I create thoughtful musical experiences for your next project`,
+    author: `Ejiro Lennox`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
