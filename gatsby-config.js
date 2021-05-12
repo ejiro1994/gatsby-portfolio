e02a13245b4253f1fdb750a047f6c8e1f2f6d911
ejiro <ejiro1994@gmail.com>
2021-05-13 00:36:55 +0100

    image

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `Ejiro Lennox`,
     description: `I create thoughtful musical experiences for your next project`,
     author: `Ejiro Lennox`,
+    logo: "./src/images/ejiro.jpg",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -12,8 +13,8 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/static/images`,
       },
-      
-    }, `gatsby-plugin-styled-components`
+    },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
